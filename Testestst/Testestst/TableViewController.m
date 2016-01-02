e0bb27353f3a0dd8e780f2a42c4d0c66435fd347
shirley <shirley@hotmail.com>
2016-01-02 16:52:26 +0800

    郝总 真棒!

--- Testestst/Testestst/TableViewController.m
+++ Testestst/Testestst/TableViewController.m
@@ -32,24 +32,25 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+//#warning Incomplete implementation, return the number of sections
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
-    return 0;
+    return 50;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
     
     // Configure the cell...
+    cell.textLabel.text = @"郝总,真棒!";
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
